86443e01fccf24a8fa89e830fbb17a12e3d29678
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2022-08-16 20:31:48 +0530

    Create InputBuffered .java

---
+++ InputBuffered .java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException; 
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        System.out.println("string : "+s);
+        int age = Integer.parseInt(reader.readLine());
+        System.out.println("integer : "+age);
+        float weight = Float.parseFloat(reader.readLine());
+        System.out.println("float : "+weight);
+        /* code */
+    }
+}
+
